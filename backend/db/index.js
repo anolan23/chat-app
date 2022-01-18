43d095a229f58865bfb4345d6f73d3d1e7c08f15
Aaron Nolan <anolan23@yahoo.com>
2022-01-17 23:19:41 -0600

    add ssl to pg pool options

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 module.exports = {
   query(text, params) {
